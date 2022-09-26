47d47ea198f1f80cee6e7163033d128c4bc956af
qianl <qianl@educ.college-montreal.qc.ca>
2022-09-26 16:17:53 -0400

    commit initial.

---
+++ TP2.py
@@ -0,0 +1,21 @@
+"""
+Programme fait par Lily Qian
+Groupe: 403
+Description: un jeu qui te permet à deviner un chiffre aléatoirement généré par l'ordinateur
+"""
+
+import random
+chiffre_aleatoire = random.randint(0 1000)
+jeu = True
+
+while jeu:
+    print("J'ai choisi un nombre entre 0 et 100. À vous de deviner...")
+    chiffre = input("Entrez votre essai")
+
+    if chiffre = chiffre_aleatoire:
+        print(vous avez gagné)
+
+
+
+
+
