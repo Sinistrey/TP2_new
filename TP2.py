5b1289584e3c553648705835f1bb1f9b00a0c6e0
qianl <qianl@educ.college-montreal.qc.ca>
2022-09-30 09:16:32 -0400

    mon nouveau code

--- TP2.py
+++ TP2.py
@@ -5,15 +5,34 @@ Description: un jeu qui te permet à deviner un chiffre aléatoirement généré
 """
 
 import random
-chiffre_aleatoire = random.randint(0 1000)
+chiffre_aleatoire = random.randint(0, 2)
 jeu = True
+nbr_essai = 0
+def nbr_essai_total():
+    global nbr_essai
+    nbr_essai += 1
 
 while jeu:
-    print("J'ai choisi un nombre entre 0 et 100. À vous de deviner...")
-    chiffre = input("Entrez votre essai")
+  print("J'ai choisi un nombre entre 0 et 2. À vous de deviner...")
+  essai = int(input("Entrez votre essai"))
 
-    if chiffre = chiffre_aleatoire:
-        print(vous avez gagné)
+  if essai == chiffre_aleatoire:
+      reponse = input(f"Bravo! Bonne réponse. Vous avez réussi en {nbr_essai_total}: Voulez-vous rejouer? (o/n)")
+      if reponse == 'n':
+          print("Merci et aurevoir...")
+          jeu = False
+
+      elif reponse == "o":
+          jeu = True
+
+
+  elif essai > chiffre_aleatoire:
+      print(f"Mauvais choix, le nombre est plus petit que {essai}")
+
+
+  elif essai < chiffre_aleatoire:
+      print(f"Mauvais choix, le nombre est plus grand que {essai}")
+
 
 
 
