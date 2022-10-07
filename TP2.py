cdf285877c10ce65bdf8cf8c1434815d207384ec
qianl <qianl@educ.college-montreal.qc.ca>
2022-10-07 15:59:13 -0400

    code final à faire verifier par prof

--- TP2.py
+++ TP2.py
@@ -5,36 +5,43 @@ Description: un jeu qui te permet à deviner un chiffre aléatoirement généré
 """
 
 import random
-chiffre_aleatoire = random.randint(0, 2)
+chiffre_aleatoire = random.randint(0, 100)
 jeu = True
-nbr_essai = 0
-def nbr_essai_total():
-    global nbr_essai
-    nbr_essai += 1
+nbr_essai_initial = 0
+
+
+def nbr_essais_total():
+   global nbr_essai_initial
+   nbr_essai_initial += 1
+"""
+
+Cette fonction sert à pouvoir montrer le nombre d'essais à la fin du jeu
+"""
+
+print("J'ai choisi un nombre entre 0 et 100. À vous de deviner...")
 
 while jeu:
-  print("J'ai choisi un nombre entre 0 et 2. À vous de deviner...")
-  essai = int(input("Entrez votre essai"))
+ essai = int(input("Entrez votre essai :"))
 
-  if essai == chiffre_aleatoire:
-      reponse = input(f"Bravo! Bonne réponse. Vous avez réussi en {nbr_essai_total}: Voulez-vous rejouer? (o/n)")
-      if reponse == 'n':
-          print("Merci et aurevoir...")
-          jeu = False
+ if essai == chiffre_aleatoire:
+     nbr_essais_total()
+     reponse = input(f"Bravo! Bonne réponse. Vous avez réussi en {nbr_essai_initial} essai(s). Voulez-vous faire une autre partie (o/n)?")
+     if reponse == 'n':
+         print("Merci et au revoir...")
+         jeu = False
 
-      elif reponse == "o":
-          jeu = True
-
-
-  elif essai > chiffre_aleatoire:
-      print(f"Mauvais choix, le nombre est plus petit que {essai}")
+     elif reponse == "o":
+         jeu = True
+         print("J'ai choisi un nombre entre 0 et 2. À vous de deviner...")
 
 
-  elif essai < chiffre_aleatoire:
-      print(f"Mauvais choix, le nombre est plus grand que {essai}")
+ elif essai > chiffre_aleatoire:
+     print(f"Mauvais choix, le nombre est plus petit que {essai}")
+     nbr_essais_total()
 
 
-
-
+ elif essai < chiffre_aleatoire:
+     print(f"Mauvais choix, le nombre est plus grand que {essai}")
+     nbr_essais_total()
 
 
