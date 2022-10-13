408c75c04c5a3adf9cba8b2d429e9b3e6b237407
qianl <qianl@educ.college-montreal.qc.ca>
2022-10-13 10:43:09 -0400

    mon nouveau code encore

--- TP2.py
+++ TP2.py
@@ -3,45 +3,46 @@ Programme fait par Lily Qian
 Groupe: 403
 Description: un jeu qui te permet à deviner un chiffre aléatoirement généré par l'ordinateur
 """
-
 import random
-chiffre_aleatoire = random.randint(0, 100)
+
+borne_minimale = int(input('Entrez le nombre minimale :'))
+borne_maximale = int(input('Entrez le nombre maximale :'))
+chiffre_aleatoire = random.randint(borne_minimale, borne_maximale)
 jeu = True
 nbr_essai_initial = 0
 
 
 def nbr_essais_total():
-   global nbr_essai_initial
-   nbr_essai_initial += 1
-"""
+    """
+    Cette fonction sert à pouvoir montrer le nombre d'essais à la fin du jeu
+    """
+    global nbr_essai_initial
+    nbr_essai_initial += 1
 
-Cette fonction sert à pouvoir montrer le nombre d'essais à la fin du jeu
-"""
 
-print("J'ai choisi un nombre entre 0 et 100. À vous de deviner...")
+print(f"J'ai choisi un nombre entre {borne_minimale} et {borne_maximale}. À vous de deviner...")
 
 while jeu:
- essai = int(input("Entrez votre essai :"))
+    essai = int(input("Entrez votre essai :"))
 
- if essai == chiffre_aleatoire:
-     nbr_essais_total()
-     reponse = input(f"Bravo! Bonne réponse. Vous avez réussi en {nbr_essai_initial} essai(s). Voulez-vous faire une autre partie (o/n)?")
-     if reponse == 'n':
-         print("Merci et au revoir...")
-         jeu = False
+    if essai == chiffre_aleatoire:
+        nbr_essais_total()
+        reponse = input(
+            f"Bravo! Bonne réponse. Vous avez réussi en {nbr_essai_initial} essai(s). Voulez-vous faire une autre partie (o/n)?")
+        if reponse == 'n':
+            print("Merci et au revoir...")
+            jeu = False
 
-     elif reponse == "o":
-         jeu = True
-         print("J'ai choisi un nombre entre 0 et 2. À vous de deviner...")
-
-
- elif essai > chiffre_aleatoire:
-     print(f"Mauvais choix, le nombre est plus petit que {essai}")
-     nbr_essais_total()
-
-
- elif essai < chiffre_aleatoire:
-     print(f"Mauvais choix, le nombre est plus grand que {essai}")
-     nbr_essais_total()
-
-
+        elif reponse == "o":
+            nbr_essai_initial = 0
+            borne_minimale = int(input('Entrez le nombre minimale :'))
+            borne_maximale = int(input('Entrez le nombre maximale :'))
+            chiffre_aleatoire = random.randint(borne_minimale, borne_maximale)
+            jeu = True
+            print(f"J'ai choisi un nombre entre {borne_minimale} et {borne_maximale}. À vous de deviner...")
+    elif essai > chiffre_aleatoire:
+        print(f"Mauvais choix, le nombre est plus petit que {essai}")
+        nbr_essais_total()
+    elif essai < chiffre_aleatoire:
+        print(f"Mauvais choix, le nombre est plus grand que {essai}")
+        nbr_essais_total()
