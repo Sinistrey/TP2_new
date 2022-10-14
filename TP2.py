647e3e3d9e54eb1e567de1efd3c307135faea2e1
qianl <qianl@educ.college-montreal.qc.ca>
2022-10-14 08:50:36 -0400

    code final (pour de vrai maintenant) à rendre

--- TP2.py
+++ TP2.py
@@ -5,19 +5,28 @@ Description: un jeu qui te permet à deviner un chiffre aléatoirement généré
 """
 import random
 
-borne_minimale = int(input('Entrez le nombre minimale :'))
-borne_maximale = int(input('Entrez le nombre maximale :'))
+
+def choix_de_bornes():
+    """
+    Cette fonction sert à permettre au joueur de définir les bornes
+    """
+    global borne_minimale, borne_maximale
+    borne_minimale = int(input('Entrez le nombre minimale :'))
+    borne_maximale = int(input('Entrez le nombre maximale :'))
+
+
+choix_de_bornes()
 chiffre_aleatoire = random.randint(borne_minimale, borne_maximale)
 jeu = True
-nbr_essai_total = 0
+nbr_essais_total = 0
 
 
 def nbr_essais_addition():
     """
     Cette fonction sert à pouvoir montrer le nombre d'essais à la fin du jeu
     """
-    global nbr_essai_total
-    nbr_essai_total += 1
+    global nbr_essais_total
+    nbr_essais_total += 1
 
 
 print(f"J'ai choisi un nombre entre {borne_minimale} et {borne_maximale}. À vous de deviner...")
@@ -28,16 +37,15 @@ while jeu:
     if essai == chiffre_aleatoire:
         nbr_essais_addition()
         reponse = input(
-            f"Bravo! Bonne réponse. Vous avez réussi en {nbr_essai_total} essai(s). Voulez-vous faire une autre partie (o/n)?")
+            f"Bravo! Bonne réponse. Vous avez réussi en {nbr_essais_total} essai(s). Voulez-vous faire une autre partie (o/n)?")
         if reponse == 'n':
             print("Merci et au revoir...")
             jeu = False
 
         elif reponse == "o":
             jeu = True
-            nbr_essai_total = 0
-            borne_minimale = int(input('Entrez le nombre minimale :'))
-            borne_maximale = int(input('Entrez le nombre maximale :'))
+            nbr_essais_total = 0
+            choix_de_bornes()
             chiffre_aleatoire = random.randint(borne_minimale, borne_maximale)
             print(f"J'ai choisi un nombre entre {borne_minimale} et {borne_maximale}. À vous de deviner...")
     elif essai > chiffre_aleatoire:
